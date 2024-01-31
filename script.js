3d9a0f9e86fa4fc995e3eadb0ed739b08c0dd0a4
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-31 18:20:50 -0300

    Refactor a little bit

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ const longBreakButton = document.querySelector('.app__card-button--longo');
 const imageBanner = document.querySelector('.app__image');
 
 focusButton.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco')
-  imageBanner.setAttribute('src', './imagens/foco.png')
-
+  alterContext('foco')
 });
 
 shortBreakButton.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto')
-  imageBanner.setAttribute('src', './imagens/descanso-curto.png')
+  alterContext('descanso-curto')
 });
 
 longBreakButton.addEventListener('click', function() {
-  html.setAttribute('data-contexto', 'descanso-longo')
-  imageBanner.setAttribute('src', './imagens/descanso-longo.png')
+  alterContext('descanso-longo')
 });
+
+function alterContext(context) {
+  html.setAttribute('data-contexto', context)
+  imageBanner.setAttribute('src', `./imagens/${context}.png`)
+}
