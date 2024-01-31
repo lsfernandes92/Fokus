662e6fd8a554785aacdf2fc636112e71f7ee4cb6
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-31 18:59:05 -0300

    Add/remove active class on buttons click

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const shortBreakButton = document.querySelector('.app__card-button--curto');
 const longBreakButton = document.querySelector('.app__card-button--longo');
 const imageBanner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
+const buttons = document.querySelectorAll('.app__card-button');
 
 focusButton.addEventListener('click', () => {
   alterContext('foco')
+  focusButton.classList.add('active');
 });
 
 shortBreakButton.addEventListener('click', () => {
   alterContext('descanso-curto')
+  shortBreakButton.classList.add('active');
 });
 
 longBreakButton.addEventListener('click', function() {
   alterContext('descanso-longo')
+  longBreakButton.classList.add('active');
 });
 
 function alterContext(context) {
+  buttons.forEach(function (context) {
+    context.classList.remove('active')
+  });
+
   html.setAttribute('data-contexto', context)
   imageBanner.setAttribute('src', `./imagens/${context}.png`)
   switch (context) {
