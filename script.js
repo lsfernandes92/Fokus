14d6899894f3b82bf79ee61bc14d0524d1702f1e
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-31 18:40:18 -0300

    Change app title on click button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focusButton = document.querySelector('.app__card-button--foco');
 const shortBreakButton = document.querySelector('.app__card-button--curto');
 const longBreakButton = document.querySelector('.app__card-button--longo');
 const imageBanner = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
 
 focusButton.addEventListener('click', () => {
   alterContext('foco')
@@ -19,4 +20,26 @@ longBreakButton.addEventListener('click', function() {
 function alterContext(context) {
   html.setAttribute('data-contexto', context)
   imageBanner.setAttribute('src', `./imagens/${context}.png`)
+  switch (context) {
+    case 'foco':
+      title.innerHTML = `
+        Otimize sua produtividade,<br>
+        <strong class="app__title-strong">mergulhe no que importa.</strong>
+      `
+      break;
+    case 'descanso-curto':
+      title.innerHTML = `
+        Que tal dar uma respirada?<br>
+        <strong class="app__title-strong">Faça uma pausa curta!</strong>
+      `
+      break;
+    case 'descanso-longo':
+      title.innerHTML = `
+        Hora de voltar à superfície.<br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>
+      `
+      break;
+    default:
+      break;
+  }
 }
