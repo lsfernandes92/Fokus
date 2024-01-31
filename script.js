33db24f567aba4798f202a4082172291bff4c122
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-31 19:34:41 -0300

    Rename consts

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const html = document.querySelector('html');
 const focusButton = document.querySelector('.app__card-button--foco');
 const shortBreakButton = document.querySelector('.app__card-button--curto');
 const longBreakButton = document.querySelector('.app__card-button--longo');
-const imageBanner = document.querySelector('.app__image');
-const title = document.querySelector('.app__title');
+const bannerImage = document.querySelector('.app__image');
+const bannerTitle = document.querySelector('.app__title');
 const buttons = document.querySelectorAll('.app__card-button');
 
 focusButton.addEventListener('click', () => {
-  alterContext('foco')
+  alterContext('foco');
   focusButton.classList.add('active');
 });
 
 shortBreakButton.addEventListener('click', () => {
-  alterContext('descanso-curto')
+  alterContext('descanso-curto');
   shortBreakButton.classList.add('active');
 });
 
 longBreakButton.addEventListener('click', function() {
-  alterContext('descanso-longo')
+  alterContext('descanso-longo');
   longBreakButton.classList.add('active');
 });
 
@@ -27,22 +27,22 @@ function alterContext(context) {
   });
 
   html.setAttribute('data-contexto', context)
-  imageBanner.setAttribute('src', `./imagens/${context}.png`)
+  bannerImage.setAttribute('src', `./imagens/${context}.png`)
   switch (context) {
     case 'foco':
-      title.innerHTML = `
+      bannerTitle.innerHTML = `
         Otimize sua produtividade,<br>
         <strong class="app__title-strong">mergulhe no que importa.</strong>
       `
       break;
     case 'descanso-curto':
-      title.innerHTML = `
+      bannerTitle.innerHTML = `
         Que tal dar uma respirada?<br>
         <strong class="app__title-strong">Faça uma pausa curta!</strong>
       `
       break;
     case 'descanso-longo':
-      title.innerHTML = `
+      bannerTitle.innerHTML = `
         Hora de voltar à superfície.<br>
         <strong class="app__title-strong">Faça uma pausa longa.</strong>
       `
