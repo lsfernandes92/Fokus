3d3b1832c3632183409d31aaeebd467a84728bef
lsfernandes92 <lsfernandes92@gmail.com>
2024-03-06 17:53:51 -0300

    Add cancel button feature and fix typo

--- script-crud.js
+++ script-crud.js
@@ -1,19 +1,25 @@
-const btnAddTask = document.querySelector('.app__button--add-task');
-const formAddTask = document.querySelector('.app__form-add-task');
-const textArea = document.querySelector('.app__form-textarea');
-const ulTasks = document.querySelector('.app__section-task-list');
+const btnAddTask = document.querySelector(".app__button--add-task");
+const btnCancelTask = document.querySelector(".app__form-footer__button--cancel");
+const formAddTask = document.querySelector(".app__form-add-task");
+const textArea = document.querySelector(".app__form-textarea");
+const ulTasks = document.querySelector(".app__section-task-list");
 
-const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function clearForm() {
+  textArea.value = "";
+  formAddTask.classList.toggle("hidden");
+}
 
 function updateTasks() {
-  localStorage.setItem('tasks', JSON.stringify(taskList));
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 }
 
 function addTaskElement(task) {
-  const li = document.createElement('li');
-  li.classList.add('app__section-task-list-item');
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
 
-  const svg = document.createElement('svg');
+  const svg = document.createElement("svg");
   svg.innerHTML = `
     <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
@@ -21,12 +27,12 @@ function addTaskElement(task) {
     </svg>
   `
 
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   p.textContent = task.description;
-  p.classList.add('app__section-task-list-item-description');
+  p.classList.add("app__section-task-list-item-description");
 
-  const btnEdit = document.createElement('button');
-  btnEdit.classList.add('app_button-edit');
+  const btnEdit = document.createElement("button");
+  btnEdit.classList.add("app_button-edit");
 
   btnEdit.onclick = () => {
     const newDescription = prompt("Enter the text you want to edit:");
@@ -37,8 +43,8 @@ function addTaskElement(task) {
     }
   };
 
-  const img = document.createElement('img');
-  img.setAttribute('src', 'imagens/edit.png');
+  const img = document.createElement("img");
+  img.setAttribute("src", "imagens/edit.png");
   btnEdit.append(img);
 
   li.append(svg);
@@ -48,11 +54,11 @@ function addTaskElement(task) {
   return li;
 }
 
-btnAddTask.addEventListener('click', () => {
-  formAddTask.classList.toggle('hidden');
+btnAddTask.addEventListener("click", () => {
+  formAddTask.classList.toggle("hidden");
 });
 
-formAddTask.addEventListener('submit', (event) => {
+formAddTask.addEventListener("submit", (event) => {
   event.preventDefault();
   const task = {
     description: textArea.value
@@ -61,9 +67,11 @@ formAddTask.addEventListener('submit', (event) => {
   const taskElement = addTaskElement(task);
   ulTasks.append(taskElement);
   updateTasks();
-  textArea.value = '';
-  formAddTask.classList.add('hidden');
+  textArea.value = "";
+  formAddTask.classList.add("hidden");
 });
+
+btnCancelTask.addEventListener("click", clearForm);
 
 taskList.forEach(task => {
   const taskElement = addTaskElement(task)
